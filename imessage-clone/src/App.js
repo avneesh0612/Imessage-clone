38a7d92a458e3395f64fa8512ae8d71329e10f04
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-01-12 21:39:57 +0530

    commit

--- imessage-clone/src/App.js
+++ imessage-clone/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import Imessage from "./Imessage";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build the iMessage Clone</h1>
-
-      
+      <Imessage />
     </div>
   );
 }
