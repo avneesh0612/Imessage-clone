d890956121829551edd59f56bf17b296a06689aa
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-01-13 17:16:03 +0530

    master

--- imessage-clone/src/Sidebar.js
+++ imessage-clone/src/Sidebar.js
@@ -3,23 +3,26 @@ import "./Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewIcon from "@material-ui/icons/RateReview";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar />
+        <Avatar className="sidebar__avatar" />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
         </div>
 
-        <IconButton  className="sidebar__inputButton">
+        <IconButton variant="outlined" className="sidebar__inputButton">
           <RateReviewIcon />
         </IconButton>
       </div>
-      <div className="sidebar__chat">
-          
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
