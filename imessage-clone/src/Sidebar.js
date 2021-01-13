f80e26e52a268f4c62c82bb0a3a08f1f9f6535b2
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-01-13 22:03:37 +0530

    commit

--- imessage-clone/src/Sidebar.js
+++ imessage-clone/src/Sidebar.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewIcon from "@material-ui/icons/RateReview";
 import SidebarChat from "./SidebarChat";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import db, { auth } from "./firebase";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar className="sidebar__avatar" />
+        <Avatar
+          onClick={() => auth.signOut()}
+          src={user.photo}
+          className="sidebar__avatar"
+        />
         <div className="sidebar__input">
           <SearchIcon />
           <input placeholder="Search" />
