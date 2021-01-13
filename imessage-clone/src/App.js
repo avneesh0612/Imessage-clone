37fc803eaef3cb197229320a2efe46d5034cce4d
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-01-13 21:36:37 +0530

    master

--- imessage-clone/src/App.js
+++ imessage-clone/src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { selectUser, login, logout } from "./features/userSlice";
 import Imessage from "./Imessage";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
-  return (
-    <div className="app">
-      <Imessage />
-    </div>
-  );
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
