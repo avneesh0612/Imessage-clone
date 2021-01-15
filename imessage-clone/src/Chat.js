5e077e271ddf16f9c579610889703ad4360a9481
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-01-15 22:16:56 +0530

    commit

--- imessage-clone/src/Chat.js
+++ imessage-clone/src/Chat.js
@@ -8,10 +8,11 @@ import { useSelector } from "react-redux";
 import db from "./firebase";
 import firebase from "firebase";
 import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
 function Chat() {
   const user = useSelector(selectUser);
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const chatName = useSelector(selectChatName);
   const chatId = useSelector(selectChatId);
   const [messages, setMessages] = useState([]);
@@ -22,16 +23,17 @@ function Chat() {
         .doc(chatId)
         .collection("messages")
         .orderBy("timestamp", "desc")
-        .onSnapshot((snapshot) => {
+        .onSnapshot((snapshot) =>
           setMessages(
             snapshot.docs.map((doc) => ({
               id: doc.id,
-              data: doc.data,
+              data: doc.data(),
             }))
-          );
-        });
+          )
+        );
     }
-  });
+  }, [chatId]);
+
   const sendMessage = (e) => {
     e.preventDefault();
 
@@ -40,24 +42,29 @@ function Chat() {
       message: input,
       uid: user.uid,
       photo: user.photo,
+      email: user.email,
       displayName: user.displayName,
     });
 
     setInput("");
   };
+
   return (
     <div className="chat">
       <div className="chat__header">
         <h4>
-          To:<span className="chat__name">{chatName}</span>
+          To: <span className="chat__name">{chatName}</span>
         </h4>
         <strong>Details</strong>
       </div>
 
+      {/* chat messages */}
       <div className="chat__messages">
-        {messages.map(({ id, data }) => (
-          <Message key={id} contents={data} />
-        ))}
+        <FlipMove>
+          {messages.map(({ id, data }) => (
+            <Message key={id} contents={data} />
+          ))}
+        </FlipMove>
       </div>
 
       <div className="chat__input">
@@ -70,6 +77,7 @@ function Chat() {
           />
           <button onClick={sendMessage}>Send Message</button>
         </form>
+
         <IconButton>
           <MicNoneIcon className="chat__mic" />
         </IconButton>
